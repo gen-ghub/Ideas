e266a47cc107ef44e58fa29245624bde96234788
gen-ghub <renbenzhuang234@gmail.com>
2020-11-14 08:56:43 +0900

    ユーザー機能とフロント追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/mypage' => 'users#mypage'
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
