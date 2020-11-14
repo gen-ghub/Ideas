7ab22fe69b2053c8fd8fe88230d2382c6be9e99f
gen-ghub <renbenzhuang234@gmail.com>
2020-11-14 09:15:59 +0900

    トップページの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to:"ideas#index"
   get '/mypage' => 'users#mypage'
   resources :users, only: [:show, :edit, :update, :destroy]
+
+  resources :ideas
 end
