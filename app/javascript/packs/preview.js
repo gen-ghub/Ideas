a7b759f1f2d3d53617fa7e76747ac3848fb7e365
gen-ghub <renbenzhuang234@gmail.com>
2020-11-16 10:45:52 +0900

    プレビュー機能の追加

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,37 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+
+    function createImageURL(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+    }
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    }
+
+    if (document.getElementById('user-image')) {
+    document.getElementById('user-image').addEventListener('change', function(e){
+      createImageURL();
+      const file =  e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+    }
+    else {
+    document.getElementById('idea-image').addEventListener('change', function(e){
+      createImageURL();
+      const file =  e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  }
+  });
+}
