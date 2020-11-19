7268416d8a927f7546cd1bd90f82b00fb66be7e4
gen-ghub <renbenzhuang234@gmail.com>
2020-11-19 09:36:37 +0900

    いいね機能非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,15 @@
 class LikesController < ApplicationController
   def create
     Like.create(user_id: current_user.id, idea_id: params[:id])
-    redirect_to idea_path
+    @idea = Idea.includes(:likes).find_by(id: params[:id])
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, idea_id: params[:id]).destroy
-    redirect_to idea_path
+    @idea = Idea.includes(:likes).find_by(id: params[:id])
   end
+
+  private
+
+
 end
