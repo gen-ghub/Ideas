7d379d4ab78aa7430266d7b368a978583ebb61d1
gen-ghub <renbenzhuang234@gmail.com>
2020-11-27 21:59:22 +0900

    マイページのフロント修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,12 +1,13 @@
 class Tag < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '提案' },
-    { id: 3, name: '募集' }
+    { id: 2, name: '規格外品' },
+    { id: 3, name: '訳あり品' },
+    { id: 4, name: 'その他' }
 
   ]
 
    include ActiveHash::Associations
-   has_many :seeds
+   has_many :ideas
 
   end
