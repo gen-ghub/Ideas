7aad348396e167b9475c9ccb7e7e22763678fd0a
gen-ghub <renbenzhuang234@gmail.com>
2020-12-01 16:36:51 +0900

    マイページフロント編集（タブ付け）

---
+++ app/javascript/packs/tab.js
@@ -0,0 +1,16 @@
+$(function() {
+  let tabs = $(".tab");
+  $(".tab").on("click", function() {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    const index = tabs.index(this);
+    $(".content").removeClass("show").eq(index).addClass("show");
+  })
+})
+
+
+$(function(){
+  $("#acMenu dt").on("click", function() {
+  $(this).next().slideToggle();
+  });
+  });
